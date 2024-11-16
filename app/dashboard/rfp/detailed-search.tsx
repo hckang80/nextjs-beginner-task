d267dd8d6d800d7957db5cdb35673f475cdf8a53
hckang80 <hckang80@gmail.com>
2024-11-16 16:09:47 +0900

    feat: 검색 영역 컴포넌트로 분리

---
+++ app/dashboard/rfp/detailed-search.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+export function DetailedSearch() {
+  return (
+    <form action="">
+      <table>
+        <tbody>
+          <tr>
+            <th>키워드세트</th>
+            <td colSpan={5}>
+              <select name="" id="">
+                <option value="">그룹을 선택하세요</option>
+              </select>
+              <button type="button">현재 세트 저장</button>
+              <button>세팅 버튼</button>
+            </td>
+          </tr>
+
+          <tr>
+            <th>사업 금액</th>
+            <td colSpan={5}>
+              <input type="number" />
+              ~
+              <input type="number" />
+              <label htmlFor="">
+                <input type="checkbox" />
+                금액 제한 없음
+              </label>
+            </td>
+          </tr>
+
+          <tr>
+            <th>공고일</th>
+            <td colSpan={5}>TODO: 달력 범위 선택</td>
+          </tr>
+
+          <tr>
+            <th>사업 구분</th>
+            <td>
+              <select name="" id="">
+                <option value="">용역</option>
+              </select>
+            </td>
+            <th>기업 제한</th>
+            <td>
+              <select name="" id="">
+                <option value="">전체 보기</option>
+              </select>
+            </td>
+            <th>정렬 기준</th>
+            <td>
+              <select name="" id="">
+                <option value="">마감일 오름차순</option>
+              </select>
+            </td>
+          </tr>
+
+          <tr>
+            <th>조건</th>
+            <td colSpan={5}>
+              <label htmlFor="">
+                <input type="checkbox" />
+                업종조건 충족
+              </label>
+              <label htmlFor="">
+                <input type="checkbox" />
+                물품조건 충족
+              </label>
+              <label htmlFor="">
+                <input type="checkbox" />
+                공동수급 허용
+              </label>
+              <label htmlFor="">
+                <input type="checkbox" />
+                실적제한 없음
+              </label>
+              <label htmlFor="">
+                <input type="checkbox" />
+                인적제한 없음
+              </label>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <button>검색하기</button>
+    </form>
+  );
+}
