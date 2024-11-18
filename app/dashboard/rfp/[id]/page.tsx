acbb1a0a26a8f38fb85d4a53e2941516dba2c5e4
hckang80 <hckang80@gmail.com>
2024-11-19 00:09:21 +0900

    feat: 공고 상세 데이터 가져오기

--- app/dashboard/rfp/[id]/page.tsx
+++ app/dashboard/rfp/[id]/page.tsx
@@ -1,9 +1,28 @@
 "use client";
 
+import { AnnouncementContext, getProducts } from "@/lib";
 import { useParams } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function RfpDetail() {
   const params = useParams<{ id: string }>();
 
-  return <div>{params.id}</div>;
+  const [data, setData] = useState<AnnouncementContext | undefined>();
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  const fetchData = async () => {
+    // TODO: 데이터를 여기서 호출하지 말고 부모에서 넘겨주는 형태로 개선 필요. 데이터 호출 최소화. Context API
+    const { products } = await getProducts(0);
+    const product = products.find((item) => item.id === +params.id);
+    setData(product);
+  };
+
+  if (!data) {
+    return "Loading...";
+  }
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
 }
