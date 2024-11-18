d3c6ffecf5a7de643269f990879e01bd2a2382ba
hckang80 <hckang80@gmail.com>
2024-11-18 13:03:46 +0900

    feat: 채널 아이템 스타일링

--- app/dashboard/rfp/(channel)/channel-search-list.tsx
+++ app/dashboard/rfp/(channel)/channel-search-list.tsx
@@ -1,4 +1,4 @@
-import { ChannelItem } from "@/lib";
+import { ChannelItem, cn } from "@/lib";
 
 export function ChannelSearchList({
   label,
@@ -39,18 +39,26 @@ export function ChannelSearchList({
       </tr>
       <tr>
         <td colSpan={2}>
-          <ul>
+          <ul className="grid gap-[5px] grid-cols-[repeat(auto-fit,_minmax(140px,_1fr))] auto-rows-fr">
             {filteredData
               .filter((item) => item.type === type)
               .map((item) => (
-                <li key={item.id} className={item.checked ? bgColor[type] : ""}>
-                  <label className="flex items-center gap-2">
+                <li key={item.id} className="contents">
+                  <label
+                    className={cn(
+                      "flex items-center gap-2 rounded-[8px] text-center h-[35px] px-[10px] text-[14px] leading-[1.2]",
+                      item.checked
+                        ? `${bgColor[type]} text-white`
+                        : "bg-gray-200"
+                    )}
+                  >
                     <input
+                      className="absolute opacity-0"
                       type="checkbox"
                       checked={item.checked}
                       onChange={() => handleItemCheckboxChange(item.id)}
                     />
-                    {item.name}
+                    <span className="grow align-item">{item.name}</span>
                   </label>
                 </li>
               ))}
