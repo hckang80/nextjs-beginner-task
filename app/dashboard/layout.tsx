fc49601174d8a00234649ffc179175cc8040b50f
hckang80 <hckang80@gmail.com>
2024-11-19 02:11:52 +0900

    feat: 주 메뉴 활성화 스타일 적용

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function DashboardLayout({
@@ -5,6 +8,12 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  console.log({ pathname });
+  const isActive = (path: string) => {
+    return pathname === path;
+  };
+
   return (
     <div className="wrap">
       <header className="global-header">
@@ -13,15 +22,29 @@ export default function DashboardLayout({
       <div className="global-inner">
         <aside className="global-aside">
           <nav className="global-nav">
-            <Link href="/dashboard">대시보드</Link>
+            <Link href="/dashboard" className="global-main-item">
+              대시보드
+            </Link>
             <details open>
-              <summary>입찰검색</summary>
-              <ul className="global-menu">
-                <li className="global-menu-item">
-                  <Link href="/dashboard/rfp">국내입찰</Link>
+              <summary className="global-main-item">입찰검색</summary>
+              <ul className="global-sub-menu">
+                <li className="global-sub-menu__item">
+                  <Link
+                    href="/dashboard/rfp"
+                    className={isActive("/dashboard/rfp") ? "is-active" : ""}
+                  >
+                    국내입찰
+                  </Link>
                 </li>
                 <li>
-                  <Link href="/dashboard/bid_favorites">관심공고</Link>
+                  <Link
+                    href="/dashboard/bid_favorites"
+                    className={
+                      isActive("/dashboard/bid_favorites") ? "is-active" : ""
+                    }
+                  >
+                    관심공고
+                  </Link>
                 </li>
               </ul>
             </details>
