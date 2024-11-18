06d503d4f0c6d603b153724c1c051a6c8425d4a0
hckang80 <hckang80@gmail.com>
2024-11-18 15:39:50 +0900

    feat: 관심공고 저장 기능 추가

--- app/dashboard/rfp/product.tsx
+++ app/dashboard/rfp/product.tsx
@@ -1,13 +1,35 @@
 import { SelectProduct } from "@/lib";
+import { Star } from "lucide-react";
 
 export function Product({
   product: { id, name, price, type, source, createAt, endAt },
 }: {
   product: SelectProduct;
 }) {
+  const saveFavorite =
+    (id: number) =>
+    (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      event.stopPropagation();
+
+      const storageKey = "myFavorites";
+      const currentFavorites: number[] = JSON.parse(
+        localStorage.getItem(storageKey) || "[]"
+      );
+      const hasFavorite = currentFavorites.includes(id);
+      const result = hasFavorite
+        ? currentFavorites.filter((uid) => uid !== id)
+        : [...currentFavorites, id];
+
+      localStorage.setItem(storageKey, JSON.stringify(result));
+    };
+
   return (
     <tr>
-      <td>TODO: 관심 공고</td>
+      <td>
+        <button onClick={saveFavorite(id)}>
+          <Star fill="hsl(var(--input))" strokeWidth={0} />
+        </button>
+      </td>
       <td>{id}</td>
       <td>{name}</td>
       <td>{price}</td>
