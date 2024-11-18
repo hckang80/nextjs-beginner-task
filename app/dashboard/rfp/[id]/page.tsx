fe0ac92b85c00976933255165c34242c319fea02
hckang80 <hckang80@gmail.com>
2024-11-19 01:42:10 +0900

    feat: 공고 상세 레이아웃 기초 공사

--- app/dashboard/rfp/[id]/page.tsx
+++ app/dashboard/rfp/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { useToast } from "@/hooks/use-toast";
 import { AnnouncementContext, getProducts, MY_FAVORITES } from "@/lib";
 import { Star } from "lucide-react";
@@ -55,17 +57,73 @@ export default function RfpDetail() {
       : "관심 공고에 추가되었습니다.";
 
   const getStateColor = (id: number) =>
-    hasFavorite(id) ? "hsl(var(--chart-4))" : "hsl(var(--input))";
+    hasFavorite(id) ? "hsl(var(--chart-4))" : "hsl(var(--border))";
 
   return (
     <div>
-      <header>
-        <h2>{data.name}</h2>
-        <button onClick={() => saveFavorite(+id)} className="p-[10px]">
+      <header className="flex items-center gap-[8px] mb-[15px]">
+        <h2 className="font-bold text-[18px]">{data.name}</h2>
+        <Button
+          onClick={() => saveFavorite(+id)}
+          className="flex items-center gap-[4px] p-[10px]"
+        >
           <Star fill={getStateColor(+id)} strokeWidth={0} />
-          {hasFavorite(+id) ? "보관중" : "관심 공고 추가"}
-        </button>
+          <span style={{ color: getStateColor(+id) }}>
+            {hasFavorite(+id) ? "보관중" : "관심 공고 추가"}
+          </span>
+        </Button>
       </header>
+
+      <div className="flex flex-col gap-[20px]">
+        <div className="flex gap-[20px]">
+          <Card className="grow basis-full">
+            <CardHeader>
+              <CardTitle>자격 분석 노트</CardTitle>
+            </CardHeader>
+          </Card>
+          <Card className="grow basis-full">
+            <CardHeader>
+              <CardTitle>프로젝트 정보</CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
+
+        <div className="flex flex-wrap lg:flex-nowrap gap-[20px]">
+          <Card className="grow basis-1/3 lg:basis-full">
+            <CardHeader>
+              <CardTitle>공동수급 ・ 지역제한 ・ 선정방식</CardTitle>
+            </CardHeader>
+          </Card>
+          <Card className="grow basis-1/3 lg:basis-full">
+            <CardHeader>
+              <CardTitle>업종제한</CardTitle>
+            </CardHeader>
+          </Card>
+          <Card className="grow shrink-0 lg:shrink basis-full">
+            <CardHeader>
+              <CardTitle>직접생산 분석 [제한 없음]</CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>담당자 정보</CardTitle>
+          </CardHeader>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>입찰참가자격</CardTitle>
+          </CardHeader>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>첨부파일</CardTitle>
+          </CardHeader>
+        </Card>
+      </div>
     </div>
   );
 }
