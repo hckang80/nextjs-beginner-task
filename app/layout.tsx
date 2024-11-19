d96481170be37f2baa160a6115a314a0b7f624df
hckang80 <hckang80@gmail.com>
2024-11-20 05:26:10 +0900

    refactor: 사스 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import './assets/styles/index.scss';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
