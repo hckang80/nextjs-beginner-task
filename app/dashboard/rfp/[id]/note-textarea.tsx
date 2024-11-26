ad103be5ac8c2cdf3da6be7e379c746d8625d3ed
hckang80 <hckang80@gmail.com>
2024-11-26 13:24:42 +0900

    refactor: 간단한 폼부터 react-hook-form으로 교체하기

--- app/dashboard/rfp/[id]/note-textarea.tsx
+++ app/dashboard/rfp/[id]/note-textarea.tsx
@@ -1,30 +1,55 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { Textarea } from '@/components/ui/textarea';
 import { useToast } from '@/hooks/use-toast';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const formSchema = z.object({
+  noteValue: z.string().min(1, '작성된 내용이 없습니다.')
+});
 
 export function NoteTextarea() {
-  const [noteValue, setNoteValue] = useState('');
   const { toast } = useToast();
 
-  const saveNoteValue = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      noteValue: ''
+    }
+  });
 
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
     toast({
       title: 'You submitted the following values:',
-      description: noteValue
+      description: (
+        <pre>
+          <code>{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      )
     });
   };
 
   return (
-    <form onSubmit={saveNoteValue}>
-      <Textarea
-        placeholder="필요한 메모를 하세요.."
-        rows={10}
-        value={noteValue}
-        onChange={(event) => setNoteValue(event.currentTarget.value)}
-      />
-      <Button className="w-full mt-[10px]">저장</Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="noteValue"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea placeholder="필요한 메모를 하세요.." rows={10} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full mt-[10px]">저장</Button>
+      </form>
+    </Form>
   );
 }
