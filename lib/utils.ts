a6f8e16c15552cce3e65cb1c22fabc0535d9c6fc
hckang80 <hckang80@gmail.com>
2024-11-27 16:03:50 +0900

    refactor: 코드 포맷 일관성 유지

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+export const MY_FAVORITES = 'myFavorites';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -9,9 +11,9 @@ export function getMaxNumber(array: number[]) {
   return !array.length ? 0 : Math.max(...array);
 }
 
-export const generatedId = (array: { id: number }[]) => {
+export function generatedId(array: { id: number }[]) {
   return getMaxNumber(array.map(({ id }) => id)) + 1;
-};
+}
 
 export function toReadableDate(date: Date | string | undefined = new Date()) {
   return new Intl.DateTimeFormat('en-CA').format(new Date(date));
@@ -25,6 +27,6 @@ export function extractNumbers(value: string) {
   return value.replace(/[^0-9]/g, '');
 }
 
-export const MY_FAVORITES = 'myFavorites';
-
-export const fetcher = async <T>(url: string): Promise<T> => fetch(url).then((res) => res.json());
+export function fetcher<T>(url: string): Promise<T> {
+  return fetch(url).then((res) => res.json());
+}
