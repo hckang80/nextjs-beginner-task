345c5978e9df8dcf026abd5dfb9c4d23f6446f2e
hckang80 <hckang80@gmail.com>
2024-12-02 15:29:23 +0900

    feat(reacy-query): 서버사이드에서 데이터 초기화

--- app/dashboard/rfp/layout.tsx
+++ app/dashboard/rfp/layout.tsx
@@ -1,9 +1,27 @@
+import { BidAnnouncementContext, fetcher, KeywordSet } from '@/lib';
 import { BidAnnouncementProvider } from './context/BidAnnouncementContext';
+import { QueryClient } from '@tanstack/react-query';
+import { headers } from 'next/headers';
 
-export default function RfpLayout({
+export default async function RfpLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headersList = await headers();
+  const { origin } = new URL(headersList.get('referer') || '');
+  const queryClient = new QueryClient();
+
+  await Promise.all([
+    queryClient.fetchQuery({
+      queryKey: ['bidAnnouncementContext'],
+      queryFn: () => fetcher<BidAnnouncementContext>(`${origin}/bidAnnouncementContext.json`)
+    }),
+    queryClient.fetchQuery({
+      queryKey: ['keywordSets'],
+      queryFn: () => fetcher<KeywordSet[]>(`${origin}/keywordSets.json`)
+    })
+  ]);
+
   return <BidAnnouncementProvider>{children}</BidAnnouncementProvider>;
 }
