9d2ce8e4f039fe4dfcb558ad3bebaed0361fbb3a
hckang80 <hckang80@gmail.com>
2024-12-13 12:13:11 +0900

    refactor(navigation): 합성 컴포넌트로 분리

--- app/dashboard/nav.tsx
+++ app/dashboard/nav.tsx
@@ -3,6 +3,7 @@
 import { cn } from '@/lib';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { ReactNode } from 'react';
 
 type Nav = {
   name: string;
@@ -15,14 +16,14 @@ const navigation: Nav[] = [
     name: '입찰검색',
     path: 'dashboard',
     children: [
-      { name: '국내입찰', path: 'rfp' },
-      { name: '관심공고', path: 'bid_favorites' }
+      { name: '국내입찰', path: 'dashboard/rfp' },
+      { name: '관심공고', path: 'dashboard/bid_favorites' }
     ]
   },
   {
     name: '산업분석',
     path: 'analysis',
-    children: [{ name: '공급기업', path: 'supplier' }]
+    children: [{ name: '공급기업', path: 'dashboard/supplier' }]
   }
 ];
 
@@ -45,21 +46,39 @@ export default function DashboardNav() {
             {name}
           </summary>
           {children && (
-            <ul className="global-sub-menu">
-              {children.map(({ name, path: depth2 }) => (
-                <li className="global-sub-menu__item" key={depth2}>
-                  <Link
-                    href={getFullPath(depth1, depth2)}
-                    className={isActive(getFullPath(depth1, depth2)) ? 'is-active' : ''}
-                  >
-                    {name}
-                  </Link>
-                </li>
+            <List>
+              {children.map((child) => (
+                <List.Item item={child} key={child.name} />
               ))}
-            </ul>
+            </List>
           )}
         </details>
       ))}
     </nav>
+  );
+}
+
+export const List = Object.assign(ListMain, {
+  Item: ListItem
+});
+
+export function ListMain({ children }: { children: ReactNode }) {
+  return <ul className="global-sub-menu">{children}</ul>;
+}
+
+export function ListItem({ item: { name, path: depth2 } }: { item: Nav }) {
+  const pathname = usePathname();
+
+  const getFullPath = (...depths: string[]) => `/${[...depths].join('/')}`;
+
+  const isActive = (path: string) => {
+    return pathname.startsWith(path);
+  };
+  return (
+    <li className="global-sub-menu__item" key={depth2}>
+      <Link href={getFullPath(depth2)} className={isActive(getFullPath(depth2)) ? 'is-active' : ''}>
+        {name}
+      </Link>
+    </li>
   );
 }
