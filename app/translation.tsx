56087ec0dde29f72feb318bf1165a86397df8bca
hckang80 <hckang80@gmail.com>
2025-02-12 12:33:17 +0900

    refactor: 불필요한 매개변수 제거

--- app/translation.tsx
+++ app/translation.tsx
@@ -3,7 +3,7 @@
 import { useTranslation } from 'next-i18next';
 
 export default function Translation() {
-  const { t } = useTranslation(['common']);
+  const { t } = useTranslation();
 
   return <p>{t('description')}</p>;
 }
